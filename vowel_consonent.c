91238f6430fb6865cdcc0a09f3ff41708e4536b3
suman kumar <sumankumar90479@gmail.com>
2026-09-24 00:17:40 +0530

    Add files via upload

---
+++ vowel_consonent.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+
+    printf("Enter alphabet:");
+    scanf("%c", &ch);
+
+    switch (ch)
+    {
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        printf("Vowel");
+        break;
+    default:
+        printf("Consonant");
+    }
+    return 0;
+}
